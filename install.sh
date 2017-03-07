39ce1095e52316231de6dd62fe25990de2bf576e
Nicolas Cuervo <nicolas.cuervo@ettus.com>
2017-03-07 02:05:14 +0100

    wip

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#/bin/sh
 # Author: Nicolas Cuervo
 # Based on install files from thoughtbot and a diversity of contributers
 
@@ -29,6 +29,12 @@ fancy_echo "Installing curl ..."
 fancy_echo "Installing zsh ..."
   sudo aptitude install -y zsh
 
+fancy_echo "Installing tmux ..."
+  sudo aptitude install -y tmux
+
+fancy_echo "Swapping ESC and Capslock ..."
+  dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:swapescape']"
+
 silver_searcher_from_source() {
     git clone git://github.com/ggreer/the_silver_searcher.git /tmp/the_silver_searcher
     sudo aptitude install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev
@@ -49,30 +55,30 @@ if ! command -v ag >/dev/null; then
     fi
 fi
 
-fancy_echo "Installing GitHub CLI client ..."
-  if [[ ! -d "$HOME/.bin/" ]]; then
-    mkdir "$HOME/.bin"
-  fi
-  curl http://hub.github.com/standalone -sLo ~/.bin/hub
-  chmod +x ~/.bin/hub
+fancy_echo "Installing oh my zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+fancy_echo "Setting up oh my zsh custom configuration..."
+  mv ~/.zshrc ~/.zshrc.orig
+  ln -s ~/.dotfiles/home/zshrc
 
 
-fancy_echo "Installing dotfiles..."
-fancy_echo "Checking current directory..."
-DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#fancy_echo "Installing dotfiles..."
+#fancy_echo "Checking current directory..."
+#DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update dotfiles itself first
-fancy_echo "Updating dotfiles from git..."
-[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+#fancy_echo "Updating dotfiles from git..."
+#[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-fancy_echo "Deleting current dotfiles"
-rm ~/.bashrc ~/.bash_profile ~/.gitconfig ~/.gitignore_global
+#fancy_echo "Deleting current dotfiles"
+#rm ~/.bashrc ~/.bash_profile ~/.gitconfig ~/.gitignore_global
 
-fancy_echo "Generating symbolic links..."
-ln -sfv "$DOTFILES_DIR/home/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
-ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
-ln -sfv "$DOTFILES_DIR/home/.bashrc" ~
+#fancy_echo "Generating symbolic links..."
+#ln -sfv "$DOTFILES_DIR/home/.bash_profile" ~
+#ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+#ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
+#ln -sfv "$DOTFILES_DIR/home/.bashrc" ~
 
 #fancy_echo "Sourcing .bashrc"
 #source ~/.bashrc
